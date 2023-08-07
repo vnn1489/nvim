7740e5e965211606dbabc80f6c8bc8a5a45e6d84
Nguyen Nguyen <116901931+vnn1489@users.noreply.github.com>
2023-08-07 21:18:54 +0700

    Update script.sh

--- shell/script.sh
+++ shell/script.sh
@@ -3,7 +3,7 @@ APT
 
 - with deb: ferdium
 
-- apti tmux vim curl ibus-unikey git snapd gh curl flameshot net-tools openssh-server xz-utils at sshpass libreoffice python3-pip
+- apti curl tmux vim ibus-unikey git snapd gh curl flameshot net-tools openssh-server xz-utils at sshpass libreoffice python3-pip
 
 code virtualbox-7.0 spotify-client
 
@@ -19,3 +19,16 @@ APPIMAGE
 - backup folder user, download, file .bashrc. vm virtual
 - export data broswer
 - save files script at the path `usr/local/bin`
+
+
+- update && upgrade
+- download file alias and save at folder user >>> push content of alias into .bashrc file
+- tmux: download file config from github and save at user folder
+- spotify: curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list && sudo apt update -y && sudo apt install -y spotify-client
+##
+
+
+SCRIPT
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install 
